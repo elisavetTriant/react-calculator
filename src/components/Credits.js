173b23c8b841b93fdde8dac89bcaa2c6c0c2cf5e
elisavetTriant <elitriant@gmail.com>
2020-12-07 20:01:26 +0200

    Adding Credits - CodeClean Up

---
+++ src/components/Credits.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Credits = () => {
+    return (
+        <div id="footer">
+            <p>Find the source code at this <a href={"https://github.com/elisavetTriant/react-calculator/"}>GitHub repository</a>.</p>
+            <p>Made with love for FreeCodeCamp, by <a href={"https://github.com/elisavetTriant/"}>@ElisavetTriant</a>.</p>
+        </div>
+    );
+}
+
+export default Credits;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './containers/Calculator';
+import Credits from './components/Credits';
 
 ReactDOM.render(
   <React.StrictMode>
+  	<React.Fragment>
     <Calculator />
+    <Credits />
+    </React.Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
